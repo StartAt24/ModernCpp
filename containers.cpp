b2bbcccf265f12adcc4c83b961efda9307579441
daijunjie <daijunjie@momenta.ai>
2020-12-14 14:22:29 +0800

    add priority_queue

--- containers.cpp
+++ containers.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <functional>
 #include <algorithm>
+#include <queue>
 #include <vector>
 #include <string>
 
@@ -17,5 +18,32 @@ void PrintVec(const vector<T>& in){
 int main(){
     vector<int> v{13, 6, 4, 11, 29};
     PrintVec<int>(v);
+
+    sort(v.begin(), v.end());
+    PrintVec<int>(v);
+
+    sort(v.begin(), v.end(), greater<int>());
+    PrintVec<int>(v);
+    cout << hex;
+
+    auto hp = hash<int*>();
+    cout << "hash(nullptr) = "<< hp(nullptr) <<endl;
+    cout << "hash(v.data()) = "<< hp(v.data()) <<endl;
+    cout << "v.data() = "<< static_cast<void*>(v.data()) <<endl;
+
+    auto hs = hash<string>();
+    cout << "hash(\"hello\")" << hs(string("hello")) <<endl;
+    cout << "hash(\"hellp\")" << hs(string("hellp")) <<endl;
+    cout << "----------" << endl;
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;
+    q.push({1,1});
+    q.push({2,2});
+    q.push({0,3});
+    q.push({9,4});
+    while(!q.empty()){
+        cout << q.top().first << ',' << q.top().second << endl;
+        q.pop();
+    }
     return 0;
 }
