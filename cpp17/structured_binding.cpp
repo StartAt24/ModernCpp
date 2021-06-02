219c2ee4c3a63018b23b8779ba0ecec9aae564a6
StartAt24 <779058143@qq.com>
2021-06-02 23:55:19 +0800

    modify structured binding

--- cpp17/structured_binding.cpp
+++ cpp17/structured_binding.cpp
@@ -8,14 +8,34 @@ struct Student{
     string name;
 };
 
+struct Base{
+virtual void interface() = 0;
+};
+
+struct Derived: public Base{
+virtual void interface() override{
+    cout << "derived" << endl;
+}
+Derived(int id, string name){
+    id_ = id;
+    name_ = name;
+}
+int id_;
+string name_;
+};
+
 int main(){
     // get value from struct.
-    auto& [id, name] = Student{3,"junjie"};
+    auto [id, name] = Student{3,"junjie"};
 
     // iterator map
     map<int, string> s;
     for(const auto& [id, name] : s){
         cout << id << name << endl;
     }
+
+    // from virtual struct
+    auto [id1, name1] = Derived{1, "hello"};
+    cout << id1 << " " << name1 << endl;
 
 }
