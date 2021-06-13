d3833a36a376ebf3abb45535af5c1cc26b34841c
StartAt24 <779058143@qq.com>
2021-06-13 23:28:36 +0800

    add initial_condition

--- cpp17/initial_condition.cpp
+++ cpp17/initial_condition.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <string>
 #include <mutex>
 using namespace std;
 mutex condition_lock;
@@ -9,6 +11,11 @@ int main(){
         cout << "a less than" << 3 << endl;
     }
 
-    
+    std::map<string, int> coll;
+    if(auto [pos, ok] = coll.insert({"new", 23}); !ok){
+        const auto&[key, val] = *pos;
+        cout << "already in there:" << key << endl;
+    }
+
 
 }
