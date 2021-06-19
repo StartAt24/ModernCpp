c0202d3fdb70f055877bc6834019f7339511c848
daijunjie <daijunjie@momenta.ai>
2021-06-19 21:15:17 +0800

    add more property

--- cpp17/constexpr_lambda.cpp
+++ cpp17/constexpr_lambda.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <thread>
+#include <string>
 #include <array>
 using namespace std;
+
+class Data{
+private:
+    std::string name;
+public:
+    Data(const string& s):name(s){}
+    auto startThreadWithCopyOfThis() const {
+        using namespace std::literals;
+        thread t([*this]{
+            this_thread::sleep_for(3s);
+            cout << name << '\n';
+        });
+        return t;
+    }
+};
 
 int main(){
     auto s = [](auto val) constexpr{
         return val*val;
     };
     array<int, s(2)> arr;
+    std::thread t;
+    {
+        Data d{"3"};
+        t = d.startThreadWithCopyOfThis();
+    }
+    t.join();
 }
--- cpp17/new_property.cpp
+++ cpp17/new_property.cpp
@@ -5,4 +5,25 @@ class MyContainer{
     [[nodiscard]] bool empty() const noexcept{
       return true;
     }
+};
+
+
+void foo(int val, [[maybe_unused]] std::string msg){
+  #ifdef DEBUG
+    std::cout << msg << std::endl;
+  #endif
+}
+
+void commentPlace(int place){
+  switch(place){
+    case 1:
+      std::cout << "very";
+      [[fallthrough]];
+    case 2:
+      std::cout << "well\n";
+      break;
+    default:
+      std::cout << "OK\n";
+      break;
+  }
 }
