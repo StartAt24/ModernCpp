d9f3142814548566686ac9644c66093629506095
daijunjie <daijunjie@momenta.ai>
2021-06-28 00:19:39 +0800

    initializer list will have a std::array inside

--- initializer_list.cpp
+++ initializer_list.cpp
@@ -1,6 +1,7 @@
 #include <initializer_list>
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 // class CC{
@@ -16,14 +17,28 @@ using namespace std;
 struct S{
 int a;
 int b;
-S(initializer_list<int> i){
-    cout << "call initializer" << endl;
-    a = *i.begin();
-    b =  *(i.end()-1);
-}
-S(int s){
+// S(initializer_list<int> i){
+//     cout << "call initializer" << endl;
+//     a = *i.begin();
+//     b =  *(i.end()-1);
+// }
+S(int s, int b){
     cout << "constructor\n";
     a = s;
+    this->b = b;
+}
+S(){
+    cout << "S constructor\n";
+}
+S(const S& other){
+    cout << "S copy\n";
+}
+
+S(S&& other){
+    cout << "S move\n";
+}
+~S(){
+    cout << "de constructor\n";
 }
 void say(){
     cout << a << " " << b << endl;
@@ -32,6 +47,31 @@ void say(){
 
 int main(){
     // S s{1, 3};
-    S s{1};
-    s.say();
+    // S s{1, 3};
+    // s.say();
+    S s1{};
+    S s2{};
+    // vector<S> s3{{},{}};
+    cout << "----\n";
+    // std::initializer_list<S> a{move(s1), move(s1) };
+    std::array<S, 3> b{move(s1), move(s2), move(s1)};
+    // vector<S> s3;
+    // s3.reserve(2);
+    // s3.emplace_back(s1);
+    // s3.emplace_back(s2);
 }
+
+// struct Foo
+// {
+// 	int x;
+// 	int y;
+// 	Foo(int, int){ cout << "Foo construction\n"; }
+// };
+ 
+// int main()
+// {
+// 	Foo foo{ 123, 321 };
+// 	cout << foo.x << " " << foo.y<< "\n";
+// 	return 0;
+ 
+// }
